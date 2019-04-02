0a28f1b7aff587d5f2bb632b86d968fd40a2f504
Lyccccc <liuyuchen_0312@163.com>
2019-04-02 14:57:07 +0800

    冒泡排序、选择排序

--- src/main/java/net/coderchen/practice/sort/SelectionSort.java
+++ src/main/java/net/coderchen/practice/sort/SelectionSort.java
@@ -3,9 +3,9 @@ package net.coderchen.practice.sort;
 /**
  * @author Liuyuchen
  * @date 2019/04/02
- * @description 冒泡排序
+ * @description 选择排序
  */
-public class BubbleSort {
+public class SelectionSort {
 
     public static void main(String[] args) {
         int[] arr = {2, 11, 5, 64, 22, 67, 99, 13, 84, 34, 64, 3, 65, 72, 56};
@@ -16,16 +16,20 @@ public class BubbleSort {
     }
 
     /**
-     * 冒泡排序实现
+     * 具体实现
      * @param arr 待排序数组
      * @return 排序结果由小到大
      */
     public static int[] sort(int[] arr){
         for(int i = 0 ; i < arr.length ; i++){
             for(int j = i + 1 ; j < arr.length ; j++){
-                int temp;
+                int k = i;
                 if(arr[i] > arr[j]){
-                    temp = arr[i];
+                    //记录下标
+                    k = j;
+                }
+                if(k != i){
+                    int temp = arr[i];
                     arr[i] = arr[j];
                     arr[j] = temp;
                 }
