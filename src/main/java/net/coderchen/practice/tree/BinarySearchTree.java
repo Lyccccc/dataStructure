bdbee9912e248f1a04fdc6c8a50fdc0a3b98635d
Lyccccc <liuyuchen_0312@163.com>
2019-09-03 17:49:19 +0800

    二叉查找树

--- src/main/java/net/coderchen/practice/tree/BinarySearchTree.java
+++ src/main/java/net/coderchen/practice/tree/BinarySearchTree.java
@@ -7,16 +7,17 @@ package net.coderchen.practice.tree;
  */
 public class BinarySearchTree {
     private Node tree;
-    private int[] arr = new int[]{61, 2, 6, 33, 10, 36, 99, 47, 21, 88, 5, 78, 16, 70, 83};
+//    private int[] arr = new int[]{61, 2, 6, 33, 10, 36, 99, 47, 21, 88, 5, 78, 16, 70, 83};
 
     public BinarySearchTree() {
-        tree = new Node(arr[0]);
-        for (int i = 1; i < arr.length; i++) {
-            buildTree(arr[i]);
-        }
+
     }
 
-    private void buildTree(int val){
+    public void insert(int val){
+        if(null == tree){
+            tree = new Node(val);
+            return;
+        }
         Node tmp = tree;
         Node node = new Node(val);
         while(true){
@@ -41,9 +42,39 @@ public class BinarySearchTree {
 
     public static void main(String[] args) {
         BinarySearchTree binarySearchTree = new BinarySearchTree();
+        binarySearchTree.insert(61);
+        binarySearchTree.insert(2);
+        binarySearchTree.insert(6);
+        binarySearchTree.insert(33);
+        binarySearchTree.insert(10);
+        binarySearchTree.insert(36);
+        binarySearchTree.insert(99);
+        binarySearchTree.insert(47);
+        binarySearchTree.insert(21);
+        binarySearchTree.insert(88);
+        binarySearchTree.insert(5);
+        binarySearchTree.insert(78);
+        binarySearchTree.insert(16);
+        binarySearchTree.insert(70);
+        binarySearchTree.insert(83);
+        System.out.println(binarySearchTree.find(1));
+        System.out.println(binarySearchTree.find(0));
+        System.out.println(binarySearchTree.find(47));
+        System.out.println(binarySearchTree.find(61));
+        System.out.println(binarySearchTree.find(10));
     }
 
     public Node find(int val){
+        Node tmp = tree;
+        while(null != tmp){
+            if(val < tmp.data){
+                tmp = tmp.left;
+            }else if(val > tmp.data){
+                tmp = tmp.right;
+            }else{
+                return tmp;
+            }
+        }
         return null;
     }
 
@@ -54,6 +85,13 @@ public class BinarySearchTree {
 
         public Node(int data) {
             this.data = data;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "data=" + data +
+                    '}';
         }
     }
 }
