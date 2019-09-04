271e3536caf7f5f05b2cde7981aa06e3075bb982
Lyccccc <liuyuchen_0312@163.com>
2019-09-04 15:02:32 +0800

    二叉树 删除节点

--- src/main/java/net/coderchen/practice/tree/BinarySearchTree.java
+++ src/main/java/net/coderchen/practice/tree/BinarySearchTree.java
@@ -18,26 +18,112 @@ public class BinarySearchTree {
             tree = new Node(val);
             return;
         }
-        Node tmp = tree;
+        Node p = tree;
         Node node = new Node(val);
         while(true){
-            while(val < tmp.data){
-                if(null != tmp.left){
-                    tmp = tmp.left;
+            while(val < p.data){
+                if(null != p.left){
+                    p = p.left;
                 }else{
-                    tmp.left = node;
+                    p.left = node;
                     return;
                 }
             }
-            while(val > tmp.data){
-                if(null != tmp.right){
-                    tmp = tmp.right;
+            while(val > p.data){
+                if(null != p.right){
+                    p = p.right;
                 }else{
-                    tmp.right = node;
+                    p.right = node;
                     return;
                 }
             }
         }
+    }
+
+    /**
+     * 删除节点
+     * @param val
+     * 三种情况：
+     * 1、若要删除的节点没有子节点，则直接将父节点指向该节点的指针置为null
+     * 2、若要删除的节点只有一个子节点，则将父节点指向该节点的指针，置为该节点的子节点
+     * 3、若要删除的节点由两个子节点，把将右子树的最小节点替换到该节点的位置上，然后删除右子树的最小节点
+     */
+    public void delele(int val){
+        //待删除的节点
+        Node p = tree;
+        //p的父节点
+        Node pp = null;
+        while(null != p){
+            if(val < p.data){
+                pp = p;
+                p = p.left;
+            }else if(val > p.data){
+                pp = p;
+                p = p.right;
+            }else{
+                break;
+            }
+        }
+        if(null == p){
+            return;
+        }
+        if(null != p.left && null != p.right){//情况3
+            //寻找右子树的最小节点
+            Node minp = p.right;
+            //右子树最小节点的父节点
+            Node minpp = p;
+            while(null != minp.left){
+                minpp = minp;
+                minp = minp.left;
+            }
+            //minP替换到p位置，然后删除minP
+            if(minpp == p){
+                p.right = minp.right;
+                p.data = minp.data;
+            }else{
+                p.data = minp.data;
+                minpp.left = minp.right;
+            }
+        }else if(null == p.left && null == p.right){//情况1
+            if(null == pp){//删除的是根节点
+                tree = null;
+                return;
+            }
+            //直接删除p
+            if(null != pp.left && p.data == pp.left.data){
+                pp.left = null;
+            }else{
+                pp.right = null;
+            }
+        }else{//情况2，左右两个子树其中一个不为空
+            if(null != p.left){
+                if(null == pp){
+                    tree = p.left;
+                }else{
+                    pp.left = p.left;
+                }
+            }else{
+                if(null == pp){
+                    tree = p.right;
+                }else{
+                    pp.right = p.right;
+                }
+            }
+        }
+    }
+
+    public Node find(int val){
+        Node p = tree;
+        while(null != p){
+            if(val < p.data){
+                p = p.left;
+            }else if(val > p.data){
+                p = p.right;
+            }else{
+                return p;
+            }
+        }
+        return null;
     }
 
     public static void main(String[] args) {
@@ -55,27 +141,12 @@ public class BinarySearchTree {
         binarySearchTree.insert(5);
         binarySearchTree.insert(78);
         binarySearchTree.insert(16);
-        binarySearchTree.insert(70);
-        binarySearchTree.insert(83);
         System.out.println(binarySearchTree.find(1));
         System.out.println(binarySearchTree.find(0));
         System.out.println(binarySearchTree.find(47));
         System.out.println(binarySearchTree.find(61));
         System.out.println(binarySearchTree.find(10));
-    }
-
-    public Node find(int val){
-        Node tmp = tree;
-        while(null != tmp){
-            if(val < tmp.data){
-                tmp = tmp.left;
-            }else if(val > tmp.data){
-                tmp = tmp.right;
-            }else{
-                return tmp;
-            }
-        }
-        return null;
+        binarySearchTree.delele(61);
     }
 
     static class Node{
